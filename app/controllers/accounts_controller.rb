26afafb06a9d14e81aad2b6294311fbbf9019352
ailtmy <takase@mitsuwa-off.jp>
2018-12-19 17:48:22 +0900

    user_model_v0.2

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,27 @@
+class AccountsController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    current_password ||= account_params[:current_password]
+
+    if @user.authenticate(current_password)
+      @user.update(account_params)
+      redirect_to account_path(@user), notice: "#{@user.name}を更新しました。"
+    else
+      render :edit
+    end
+
+  end
+
+  private
+  def account_params
+    params.require(:user).permit(:name, :email, :current_password, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   
+  attr_accessor :current_password
+  validates :password, presence: { if: :current_password }
 end
